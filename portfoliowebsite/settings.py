4007be64734fe42ab6d1e3c6833d1dc2dd305fc5
hadh2 <hadh2@scb.com.vn>
2023-12-11 16:57:19 +0700

    add aws eb to allowed_hosts

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -16,7 +16,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['web-production-3b14.up.railway.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['web-production-3b14.up.railway.app', '127.0.0.1', 'localhost',
+                 'danghabeanstalk-dev.us-east-1.elasticbeanstalk.com']
 
 # Application definition
 
